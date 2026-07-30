0616ce770c3d27df00fee911600900d851f0c198
mrlindzer3 <mr.lindzer3@gmail.com>
2026-07-30 08:36:00 -0700

    Create vellum_grid/__init__.py

---
+++ vellum_grid/__init__.py
@@ -0,0 +1,5 @@
+from vellum_grid.core.generator import VellumGridEngine
+from vellum_grid.data.lexicon_matrix import LEXICON_MATRIX
+
+__version__ = "0.1.0"
+__all__ = ["VellumGridEngine", "LEXICON_MATRIX"]
